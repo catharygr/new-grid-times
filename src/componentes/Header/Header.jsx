9b60527b9c67c807d6488bfbeaa4a3f5a06ce37d
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-10 14:56:18 +0000

    Trabajando en el componente Logo.jsx

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,11 +1,22 @@
 import styles from "./Header.module.css";
+import { Menu, Search, User } from "react-feather";
 
 export default function Header() {
   return (
     <header>
       <div className={styles.superHeader}>
-        <div className={styles.row}></div>
+        <div className={styles.row}>
+          <div className={styles.actionGroup}>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </div>
+        </div>
       </div>
+      <div className={styles.mainHeader}></div>
     </header>
   );
 }
