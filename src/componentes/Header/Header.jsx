a4712c6726b72eac8c371b525221add3a83db469
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-10 14:09:41 +0000

    Trabajando en el componente Header.jsx

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -3,7 +3,9 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header>
-      <div className={styles.superHeader}>Header</div>
+      <div className={styles.superHeader}>
+        <div className={styles.row}></div>
+      </div>
     </header>
   );
 }
