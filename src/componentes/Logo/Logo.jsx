7d805548b61a033dda7975084772701adea2bf32
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-11 09:33:12 +0000

    Trabajando en el componente Logo.module.css

--- src/componentes/Logo/Logo.jsx
+++ src/componentes/Logo/Logo.jsx
@@ -1,1 +1,17 @@
-export default function Logo() {}
+import { format } from "date-fns";
+import styles from "./Logo.module.css";
+import Link from "next/link";
+
+export default function Logo(prosp) {
+  return (
+    <div className={styles.wrapper}>
+      <Link
+        href="/"
+        {...prosp}
+      >
+        New Grid Times
+      </Link>
+      <p className={styles.todaysDate}></p>
+    </div>
+  );
+}
