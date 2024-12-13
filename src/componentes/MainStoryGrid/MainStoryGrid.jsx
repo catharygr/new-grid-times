0e8052bbf4144b26c221092f825f66822e4f79c5
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-13 10:00:25 +0000

    Terminado el componente MainStoryGrid.jsx

--- src/componentes/MainStoryGrid/MainStoryGrid.jsx
+++ src/componentes/MainStoryGrid/MainStoryGrid.jsx
@@ -1,6 +1,10 @@
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from "@/data";
+import styles from "./MainStoryGrid.module.css";
+
 export default function MainStoryGrid() {
   return (
-    <div>
+    <div className={styles.wrapper}>
+      <section></section>
       <h1>MainStoryGrid</h1>
     </div>
   );
