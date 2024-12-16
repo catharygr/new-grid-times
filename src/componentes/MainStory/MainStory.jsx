042d7f445be1f95876485865b6fcd67112fd77bb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-16 10:11:09 +0000

    Terminado el componente MainStory.module.css

--- src/componentes/MainStory/MainStory.jsx
+++ src/componentes/MainStory/MainStory.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import styles from "./MainStory.module.css";
 import Image from "next/image";
+import gamePad from "../../../public/images/gamepad.jpg";
+import gamePad2 from "../../../public/images/gamepad@2x.jpg";
 
 export default function MainStory({
   id,
@@ -13,12 +15,28 @@ export default function MainStory({
   return (
     <div {...delegated}>
       <Link href={`/story/${id}`}>
-        <Image
-          src={image.src}
-          alt={image.alt}
-        />
+        <picture>
+          <source
+            srcSet={gamePad2.src}
+            media="(max-width: 47.999rem)"
+          />
+          <source
+            media="(min-width: 48rem)"
+            srcSet={gamePad.src}
+          />
+          <Image
+            src={gamePad.src}
+            alt={image.alt}
+            width={gamePad.width}
+            height={gamePad.height}
+          />
+        </picture>
         <h2>{title}</h2>
       </Link>
+      <p>
+        <span>{location}</span> - {abstract}
+      </p>
+      <Link href="/story">Continúa leyendo...</Link>
     </div>
   );
 }
--- src/componentes/MainStoryGrid/MainStoryGrid.jsx
+++ src/componentes/MainStoryGrid/MainStoryGrid.jsx
@@ -1,11 +1,13 @@
 import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from "@/data";
 import styles from "./MainStoryGrid.module.css";
+import MainStory from "../MainStory/MainStory";
 
 export default function MainStoryGrid() {
   return (
     <div className={styles.wrapper}>
-      <section></section>
-      <h1>MainStoryGrid</h1>
+      <section>
+        <MainStory {...MAIN_STORY} />
+      </section>
     </div>
   );
 }
