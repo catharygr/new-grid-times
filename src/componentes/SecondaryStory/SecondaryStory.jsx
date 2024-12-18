7ea80da291129b8ca858857044f8910839819ffb
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-18 12:54:56 +0000

    Trabajando en el componente SencodaryStory.jsx

--- src/componentes/SecondaryStory/SecondaryStory.jsx
+++ src/componentes/SecondaryStory/SecondaryStory.jsx
@@ -5,6 +5,8 @@ import politiciansBig from "../../../public/images/politicians@2x.png";
 import politicians from "../../../public/images/politicians.jpg";
 import oldCarBig from "../../../public/images/old-car@2x.jpg";
 import oldCar from "../../../public/images/old-car.jpg";
+import fashionBig from "../../../public/images/fashion@2x.jpg";
+import fashion from "../../../public/images/fashion.jpg";
 
 export default function SecondaryStory({
   id,
@@ -34,12 +36,12 @@ export default function SecondaryStory({
             srcSet={oldCar.src}
           />
           <source
-            srcSet={politiciansBig.src}
+            srcSet={fashionBig.src}
             media="(max-width: 47.999rem)"
           />
           <source
             media="(min-width: 48rem)"
-            srcSet={politicians.src}
+            srcSet={fashion.src}
           />
           <Image
             className={styles.img}
@@ -47,6 +49,20 @@ export default function SecondaryStory({
             alt={image.alt}
             width={politicians.width}
             height={politicians.height}
+          />
+          <Image
+            className={styles.img}
+            src={oldCar.src}
+            alt={image.alt}
+            width={oldCar.width}
+            height={oldCar.height}
+          />
+          <Image
+            className={styles.img}
+            src={fashion.src}
+            alt={image.alt}
+            width={fashion.width}
+            height={fashion.height}
           />
         </picture>
         <h3>{title}</h3>
