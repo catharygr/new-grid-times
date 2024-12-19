e684326a521978fcff01b335cf8dc157c37f19c0
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-19 11:05:00 +0000

    Trabajando en SectionTitle.module.css

--- src/componentes/SectionTitle/SectionTitle.jsx
+++ src/componentes/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function SectionTitle({ children, cornerLink }) {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>{children}</h2>
+      {cornerLink && (
+        <Link
+          className={styles.a}
+          href={cornerLink.href}
+        >
+          {cornerLink.content}
+        </Link>
+      )}
     </div>
   );
 }
