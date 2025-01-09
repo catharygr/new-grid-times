a9c040db5f6cec10e6bc3a50dda2e9f6cf8a325d
catharygr <103951447+catharygr@users.noreply.github.com>
2025-01-09 14:53:26 +0000

    Trabajando en  VisuallyHidden.jsx con el hook useEffect()

--- src/componentes/VisuallyHidden/VisuallyHidden.jsx
+++ src/componentes/VisuallyHidden/VisuallyHidden.jsx
@@ -1,8 +1,34 @@
 import styles from "./VisuallyHidden.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function VisuallyHidden({ children, ...delegated }) {
   const [forceShow, setForceShow] = useState(false);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "production") {
+      const handleKeyDown = (ev) => {
+        if (ev.key === "Alt") {
+          setForceShow(true);
+        }
+      };
+
+      const handleKeyUp = () => {
+        setForceShow(false);
+      };
+
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("keyup", handleKeyUp);
+
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+        window.removeEventListener("keydown", handleKeyUp);
+      };
+    }
+  }, []);
+
+  if (forceShow) {
+    return children;
+  }
   return (
     <div
       className={styles.wrapper}
