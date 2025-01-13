c328c68a7d7ee38c8ccf05715722b192af77ef16
catharygr <103951447+catharygr@users.noreply.github.com>
2025-01-13 10:37:50 +0000

    Trabajando en Footer.module.css

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -5,7 +5,11 @@ export default function Footer() {
   return (
     <footer className={styles.wrapper}>
       <div className={styles.maxWidthWrapper}>
-        <div className={styles.topRow}></div>
+        <div className={styles.topRow}>
+          <nav>
+            <ul className={styles.topNavList}></ul>
+          </nav>
+        </div>
       </div>
     </footer>
   );
