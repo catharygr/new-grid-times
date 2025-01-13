d64005eb504674d93246f978a71931f452c542e7
catharygr <103951447+catharygr@users.noreply.github.com>
2025-01-13 10:21:54 +0000

    Trabajando en el componente Footer.jsx

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.wrapper}>
+      <div className={styles.maxWidthWrapper}></div>
       <p>Footer</p>
     </footer>
   );
